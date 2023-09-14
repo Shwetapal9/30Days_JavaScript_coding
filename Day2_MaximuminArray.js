60310f061246a671a86df387f85aa3e3bc6be8e3
ShwetaPal <shwetapal975@gmail.com>
2023-09-14 23:54:56 +0530

    Day2

---
+++ Day2_MaximuminArray.js
@@ -0,0 +1,14 @@
+// Find the Maximum number in a given Array
+let arr = [3, 5, 9, 11, 35, 89];
+// Approach_1: Using IN-built-Math.max Func and spread operator
+
+const maxMath = Math.max(...arr);
+console.log(maxMath);
+
+//Approach_2: Using reduce method
+const maxReduce = arr.reduce((max, curr) => (curr > max ? curr : max), arr[0]);
+console.log(maxReduce);
+
+//Approach_3: Using sor() method
+const maxSort = arr.sort((a, b) => b - a)[0];
+console.log(maxSort);
